4cd9aa4816a7aea022fa582aa5743b14fd8435f9
jonaznas <git@jonaz.dev>
2022-01-19 19:41:35 +0100

    feat: add settings page with layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { OverviewComponent } from 'src/app/overview/overview.component';
 import { LayoutComponent } from 'src/app/layout/layout.component';
 import { QuizResolver } from 'src/app/quiz/quiz.resolver';
 import { QuizItemComponent } from 'src/app/quiz/quiz-item/quiz-item.component';
-import { QuizQuestionComponent } from 'src/app/quiz/quiz-question/quiz-question.component';
+import { SettingsComponent } from 'src/app/settings/settings.component';
 
 const routes: Routes = [
   {
     path: 'overview',
     component: OverviewComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
   },
   {
     path: 'quiz/:id',
