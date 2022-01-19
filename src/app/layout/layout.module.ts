a2f8b68a1f97d0456554ff9f2e5c7a9654574e7c
jonaznas <git@jonaz.dev>
2022-01-19 19:21:13 +0100

    feat: add bottom navbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from 'src/app/layout/layout.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IconModule } from 'src/app/layout/icons/icon.module';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    IconModule
   ]
 })
 export class LayoutModule {
