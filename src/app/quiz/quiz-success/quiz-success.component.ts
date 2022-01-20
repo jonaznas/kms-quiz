2a9130ee92686fd193932df8dd7f18cb712ad6e6
jonaznas <git@jonaz.dev>
2022-01-20 09:54:35 +0100

    feat: add quiz success page

---
+++ src/app/quiz/quiz-success/quiz-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-success',
+  templateUrl: './quiz-success.component.html',
+  styleUrls: ['./quiz-success.component.scss']
+})
+export class QuizSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
