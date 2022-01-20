678fefbfaf72ecb13d0f53685cb690fcb1fb0fd9
jonaznas <git@jonaz.dev>
2022-01-20 10:55:07 +0100

    feat: implement data deletion in settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/app/quiz/quiz.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private quizService: QuizService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  deleteAllData(): void {
+    this.quizService.deleteStorageData();
+  }
 }
